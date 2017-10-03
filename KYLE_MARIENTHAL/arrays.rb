cf9dbad9df41728c0bb6b320e8d3d10fc4fee0dd
Kyle Marienthal <kylemarienthal@gmail.com>
2017-10-03 13:59:30 -0500

    arrays, guess the number, intro to ruby, iterators, range

---
+++ KYLE_MARIENTHAL/arrays.rb
@@ -0,0 +1,16 @@
+arr = [1, "two", 3.0, 4, 65, "dogg", "trains", {'kyle': 31}, ['kyle', "marienthal"]]
+# arr2 = [1,2,4,8,2,6,5,3,2,7,8,9]
+
+# puts arr.at 1
+# puts arr.fetch -1
+# puts arr.delete 'dogg'
+# puts arr.reverse.to_s
+# puts arr.length
+# puts arr2.sort.to_s
+# puts arr2.slice 6
+# puts arr.shuffle
+# puts arr.join "-JOIN-"
+# puts arr.insert (1,55555) #i don't get this one
+# puts arr.values_at(4, 65) # or this one
+# puts arr
+# puts arr2
---
+++ KYLE_MARIENTHAL/range.rb
@@ -0,0 +1,10 @@
+arr=["ant", "bear", "cat"]
+arr2=[5, 4, 3]
+
+p arr.include?("cat")
+
+p arr.last
+
+p arr2.max
+
+p arr2.min
