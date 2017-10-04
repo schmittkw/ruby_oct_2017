57a7f0eed730ffa4d0e31d956ba1111ab70a01ec
Alyssa McKee <alyssa_mckee@yahoo.com>
2017-10-03 20:53:28 -0500

    changed human to make it more friendly to my child classes, created wizards, samurai, and ninja

--- Alyssa_McKee/oop/human.rb
+++ Alyssa_McKee/oop/human.rb
@@ -1,17 +1,17 @@
 class Human
 	attr_reader :strength, :intelligence, :stealth, :health
-	def initialize
-		@strength = 3
-		@stealth = 3
-		@intelligence = 3
-		@health = 100
+	def initialize strength: 3, stealth: 3, intelligence: 3, health: 100
+		@strength = strength
+		@stealth = stealth
+		@intelligence = intelligence
+		@health = health
 	end
 	def attack target
 		if target.class <= Human #if target's class is a subclass or same class as Human
 			target.damaged @strength  
-			puts "attack sucessfull"
-		else
-			puts "attack unsucessfull"
+			#puts "attack sucessfull"
+		#else
+			#puts "attack unsucessfull"
 		end
 	end
 	def damaged amount
@@ -21,17 +21,17 @@ class Human
 		@health += amount
 	end
 	def to_s
-		"Human strength:#{@stealth} intelligence:#{@intelligence} stealth:#{@stealth} health: #{@health}"
+		"#{self.class}: strength:#{@stealth} intelligence:#{@intelligence} stealth:#{@stealth} health: #{@health}"
 	end
 end
 
-human1 = Human.new
-human2 = Human.new
-puts human1
-puts human2
-human1.attack human2
-puts human1
-puts human2
-human1.attack "rock"
-puts human1
-puts human2
+# human1 = Human.new
+# human2 = Human.new
+# puts human1
+# puts human2
+# human1.attack human2
+# puts human1
+# puts human2
+# human1.attack "rock"
+# puts human1
+# puts human2
