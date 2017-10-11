e9d3785f496b6ea0aa1a6ed0d4f87b53c13a56d6
Alyssa McKee <alyssa_mckee@yahoo.com>
2017-10-11 14:37:12 -0500

    added create functionality

--- Alyssa_McKee/rails/dojodash/app/controllers/dojos_controller.rb
+++ Alyssa_McKee/rails/dojodash/app/controllers/dojos_controller.rb
@@ -2,4 +2,20 @@ class DojosController < ApplicationController
 	def index
 		@dojos = Dojo.all
 	end	
+	def new
+		
+	end
+	def create
+		dojo = Dojo.new(dojo_params)
+		if dojo.valid?
+			dojo.save()
+			return redirect_to "/dojos"
+		end
+		flash[:notice] = dojo.errors.full_messages
+		return redirect_to "/dojos/new"
+	end
+	private
+		def dojo_params
+			return params.require(:dojos).permit(:branch, :street, :city, :state)
+		end
 end
--- Alyssa_McKee/rails/dojodash/config/routes.rb
+++ Alyssa_McKee/rails/dojodash/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get 'dojos' => 'dojos#index'
+  post 'dojos' => 'dojos#create'
+  get 'dojos/new' => 'dojos#new'
+  
 end
