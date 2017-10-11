38252085db99a86101f2740be6dcad9dfce2ddc6
Alyssa McKee <alyssa_mckee@yahoo.com>
2017-10-11 15:08:42 -0500

    added show, edit and delete methods

--- Alyssa_McKee/rails/dojodash/config/routes.rb
+++ Alyssa_McKee/rails/dojodash/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'dojos' => 'dojos#index'
+  get 'dojos' => 'dojos#index', as: "landing"
   post 'dojos' => 'dojos#create'
   get 'dojos/new' => 'dojos#new'
   
+  
+  get 'dojos/:id' => 'dojos#show'
+  patch 'dojos/:id' => 'dojos#update'
+  get 'dojos/:id/edit' => 'dojos#edit'
+  delete 'dojos/:id/destroy' => 'dojos#destroy'
 end
