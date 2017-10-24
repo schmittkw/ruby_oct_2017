222d7a0771fcfb2abff13930ebf32b9dcad39a0f
KEVIN SCHMITT <kschmit1@aisd.net>
2017-10-23 22:18:01 -0500

    fixed dojo secrets

--- Kevin_Schmitt/dojo_secrets/app/models/user.rb
+++ Kevin_Schmitt/dojo_secrets/app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  has_many :secrets
+  has_many :likes, dependent: :destroy
+  has_many :secrets_liked, through: :likes, source: :secret
+
   email_regex = /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/
   before_save :downcase
   validates :name, presence: true
